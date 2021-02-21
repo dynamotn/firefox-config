457e150732c7bf298363bf98ab5f5dc16a191b39
Trần Đức Nam <dynamo.foss@gmail.com>
2021-02-21 10:36:49 +0700

    Refactor link when setup

--- setup.sh
+++ setup.sh
@@ -2,39 +2,63 @@
 DIR=$(dirname "$(readlink -f "$0")")
 PROFILE_NAME=Dynamo
 
+# Get Profile folder
 _get_profile() {
   find "${HOME}/.mozilla/firefox/" -maxdepth 1 -type d -regextype egrep -regex '.*[a-zA-Z0-9]+.'$PROFILE_NAME
+}
+
+# Create symbolic link for config
+# $1: The source of symlink
+# $2: The destination of symlink
+# $3: When true, create parent directories to the link as needed. (default: true)
+# $4: Force removes the old destination, file or folder, and forces a new link (default: true)
+_link() {
+  local need_create_parent=${3:-true}
+  local is_force=${4:-true}
+
+  if [[ -e "$2" || -L "$2" ]]; then
+  # Check destination is existed or is a symlink
+    if test "$1" -ef "$(readlink -f "$2")"; then
+    # If symlink is setup same with source then finish
+      echo "${2} linked"
+      return
+    else
+    # Delete destination if not a symlink or target of symlink is not same with source
+      [ "$is_force" = true ] && rm -rf "$2"
+    fi
+  fi
+  [ "$need_create_parent" = true ] && mkdir -p "$(dirname "$2")"
+  ln -sv "$1" "$2"
 }
 
 if ! type firefox > /dev/null; then
   exit
 fi
 
-if [ -z $(_get_profile) ]; then
+if [ -z "$(_get_profile)" ]; then
   firefox -CreateProfile $PROFILE_NAME
 fi
 profile_folder=$(_get_profile)
 echo "Profile folder is $profile_folder"
 
 echo "Setting user preference"
-curl -SL https://raw.githubusercontent.com/arkenfox/user.js/master/user.js -o $profile_folder/user.js
-cat $DIR/user.js >> $profile_folder/user.js
+curl -SL https://raw.githubusercontent.com/arkenfox/user.js/master/user.js -o "$profile_folder/user.js"
+cat "$DIR/user.js" >> "$profile_folder/user.js"
 
 echo "Setting theme"
-rm -rf $profile_folder/chrome
-ln -s $DIR/chrome $profile_folder/chrome
+_link "$DIR/chrome" "$profile_folder/chrome"
 
 echo "Restore search engines"
-ln -s $DIR/search.json $profile_folder/search.json.mozlz4
+_link "$DIR/search.json" "$profile_folder/search.json.mozlz4"
 
 echo "Restore containers"
-ln -s $DIR/containers.json $profile_folder/containers.json
+_link "$DIR/containers.json" "$profile_folder/containers.json"
 
 echo "Restore bookmarks"
-ln -s $DIR/sensitive/bookmarks $profile_folder/bookmarkbackups
+_link "$DIR/sensitive/bookmarks" "$profile_folder/bookmarkbackups"
 
 echo "Restore site preferences"
-ln -s $DIR/sensitive/site-prefs/permissions.sqlite $profile_folder/permissions.sqlite
-ln -s $DIR/sensitive/site-prefs/content-prefs.sqlite $profile_folder/content-prefs.sqlite
+_link "$DIR/sensitive/site-prefs/permissions.sqlite" "$profile_folder/permissions.sqlite"
+_link "$DIR/sensitive/site-prefs/content-prefs.sqlite" "$profile_folder/content-prefs.sqlite"
 
 echo "Done"
